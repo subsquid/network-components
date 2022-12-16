7e5183f73d6fa3c948e1379c4ab923024a9cefe2
tmcgroul <safonov.a.r@mail.ru>
2022-12-16 03:44:21 +0300

    Change web server address to zeros

--- crates/archive-router-api/src/server.rs
+++ crates/archive-router-api/src/server.rs
@@ -84,7 +84,7 @@ impl Server {
             .route("/metrics", get(get_metrics))
             .layer(from_fn(logging))
             .layer(Extension(self.router.clone()));
-        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+        let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
         axum::Server::bind(&addr)
             .serve(app.into_make_service())
             .await

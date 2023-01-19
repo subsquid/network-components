8032bde38da2286a88920f9e471691cb5da7a6f8
Eldar Gabdullin <eldargab@gmail.com>
2023-01-19 19:36:28 +0300

    fix RangeSet::from()

--- crates/router-controller/src/range.rs
+++ crates/router-controller/src/range.rs
@@ -1,5 +1,4 @@
-use std::cmp::Ordering;
-use std::fmt::write;
+use std::cmp::{max, Ordering};
 use serde::{Serialize, Deserialize};
 
 
@@ -73,12 +72,15 @@ impl From<Vec<Range>> for RangeSet {
         let mut pi = 0;
         for i in 1..ranges.len() {
             let c = ranges[i];
-            let mut p = ranges[pi];
-            if c.begin() > p.end() {
+            let p = ranges[pi];
+            if c.begin() > p.end() + 1 {
                 pi += 1;
                 ranges[pi] = c;
             } else {
-                p.1 = c.end();
+                ranges[pi] = Range(
+                    p.begin(),
+                    max(c.end(), p.end())
+                );
             }
         }
         ranges.truncate(pi + 1);
@@ -90,5 +92,60 @@ impl From<Vec<Range>> for RangeSet {
 impl std::fmt::Debug for RangeSet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "RangeSet{:?}", self.0)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::range::{Range, RangeSet};
+
+    #[test]
+    fn range_set_always_sorted_and_separated() {
+        let rs = RangeSet::from(vec![
+            Range(10, 20),
+            Range(21, 30),
+            Range(35, 40),
+            Range(5, 10)
+        ]);
+        assert_eq!(rs.0, vec![
+            Range(5, 30),
+            Range(35, 40)
+        ].into());
+    }
+
+    #[test]
+    fn range_set_has() {
+        let rs = RangeSet::from(vec![
+            Range(0, 10),
+            Range(20, 30),
+            Range(40, 50)
+        ]);
+
+        assert!(rs.has(0));
+        assert!(rs.has(5));
+        assert!(rs.has(10));
+        assert!(rs.has(21));
+        assert!(rs.has(40));
+        assert!(rs.has(41));
+        assert!(rs.has(50));
+        assert!(!rs.has(11));
+        assert!(!rs.has(19));
+        assert!(!rs.has(35));
+    }
+
+    #[test]
+    fn range_set_find_containing_range() {
+        let rs = RangeSet::from(vec![
+            Range(0, 10),
+            Range(20, 30),
+            Range(40, 50)
+        ]);
+
+        assert_eq!(rs.find_containing_range(0), Some(Range(0, 10)));
+        assert_eq!(rs.find_containing_range(5), Some(Range(0, 10)));
+        assert_eq!(rs.find_containing_range(30), Some(Range(20, 30)));
+        assert_eq!(rs.find_containing_range(41), Some(Range(40, 50)));
+        assert_eq!(rs.find_containing_range(15), None);
     }
 }

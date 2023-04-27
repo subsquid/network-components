ea20954c84133ca7fd2cd53dce31e0a68f77af77
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2023-04-27 12:11:13 +0200

    Streaming active worker set updates

--- crates/contract-client/examples/get_active_workers.rs
+++ crates/contract-client/examples/get_active_workers.rs
@@ -1,9 +1,18 @@
 use contract_client::Client;
+use simple_logger::SimpleLogger;
+use tokio_stream::StreamExt;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .env()
+        .init()?;
+
     let client = Client::new("http://127.0.0.1:8545/")?;
-    let workers = client.get_active_workers().await?;
-    workers.iter().for_each(|w| println!("{w:?}"));
+    let mut worker_stream = client.active_workers_stream().await;
+    while let Some(workers) = worker_stream.next().await {
+        workers.iter().for_each(|w| println!("{w:?}"));
+    }
     Ok(())
 }
--- crates/contract-client/src/lib.rs
+++ crates/contract-client/src/lib.rs
@@ -1,9 +1,12 @@
-use ethers::prelude::{abigen, ContractError};
+use ethers::prelude::{abigen, ContractError, Middleware};
 use ethers::providers::{Http, Provider};
 use ethers::types::{Address, U256};
+use futures::{Stream, StreamExt};
 use lazy_static::lazy_static;
 use libp2p::PeerId;
 use std::sync::Arc;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
 
 abigen!(
     TSQD,
@@ -28,6 +31,7 @@ lazy_static! {
 }
 
 #[allow(dead_code)]
+#[derive(Clone)]
 pub struct Client {
     tsqd: TSQD<Provider<Http>>,
     worker_registration: WorkerRegistration<Provider<Http>>,
@@ -41,27 +45,40 @@ pub enum ClientError {
     InvalidPeerId(#[from] libp2p::multihash::Error),
     #[error("Contract error: {0:?}")]
     Contract(#[from] ContractError<Provider<Http>>),
+    #[error("RPC provider error: {0:?}")]
+    Provider(#[from] ethers::providers::ProviderError),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Worker {
     pub peer_id: PeerId,
     pub address: Address,
     pub bond: U256,
     pub registered_at: U256,
-    pub deregistered_at: U256,
+    pub deregistered_at: Option<U256>,
 }
 
 impl TryFrom<worker_registration::Worker> for Worker {
     type Error = ClientError;
 
     fn try_from(worker: worker_registration::Worker) -> Result<Self, Self::Error> {
+        // Strip the padding – all but last trailing zeros
+        let peer_id_bytes: Vec<u8> = worker.peer_id.concat();
+        let last_trailing_zero = peer_id_bytes
+            .iter()
+            .position(|x| *x != 0)
+            .ok_or(libp2p::multihash::Error::InvalidSize(0))?
+            .saturating_sub(1);
+        let peer_id = PeerId::from_bytes(&peer_id_bytes[last_trailing_zero..])?;
+
+        let deregistered_at = (worker.deregistered_at > 0.into()).then_some(worker.deregistered_at);
+
         Ok(Self {
-            peer_id: PeerId::from_bytes(worker.peer_id.concat().as_slice())?,
+            peer_id,
             address: worker.account,
             bond: worker.bond,
             registered_at: worker.registered_at,
-            deregistered_at: worker.deregistered_at,
+            deregistered_at,
         })
     }
 }
@@ -79,7 +96,8 @@ impl Client {
         })
     }
 
-    pub async fn get_active_workers(&self) -> Result<Vec<Worker>, ClientError> {
+    /// Get current active worker set
+    pub async fn active_workers(&self) -> Result<Vec<Worker>, ClientError> {
         self.worker_registration
             .get_active_workers()
             .call()
@@ -87,5 +105,66 @@ impl Client {
             .into_iter()
             .map(|worker| worker.try_into())
             .collect()
+    }
+
+    /// Get a stream which yields an updated set of active workers after every change
+    pub async fn active_workers_stream(&self) -> impl Stream<Item = Vec<Worker>> + 'static {
+        let client = self.clone();
+        let (tx, rx) = mpsc::channel(100);
+        let updater = WorkerSetUpdater::new(client, tx);
+        tokio::spawn(updater.run());
+        ReceiverStream::new(rx)
+    }
+}
+
+struct WorkerSetUpdater {
+    client: Client,
+    result_sender: mpsc::Sender<Vec<Worker>>,
+    last_worker_set: Option<Vec<Worker>>,
+}
+
+impl WorkerSetUpdater {
+    pub fn new(client: Client, result_sender: mpsc::Sender<Vec<Worker>>) -> Self {
+        Self {
+            client,
+            result_sender,
+            last_worker_set: None,
+        }
+    }
+
+    pub async fn run(mut self) -> () {
+        let raw_client = self.client.worker_registration.client();
+        let mut block_stream = match raw_client.watch_blocks().await {
+            Ok(stream) => stream,
+            Err(e) => return log::error!("Cannot get blocks: {e:?}"),
+        };
+
+        // Send active worker set immediately and then check for updates every new block
+        loop {
+            if self.next_update().await.is_err() {
+                break; // Stream receiver was dropped
+            }
+            if block_stream.next().await.is_none() {
+                break; // Block stream ended -> connection error(?)
+            }
+        }
+    }
+
+    async fn next_update(&mut self) -> Result<(), mpsc::error::SendError<Vec<Worker>>> {
+        match (
+            self.client.active_workers().await,
+            &mut self.last_worker_set,
+        ) {
+            (Err(e), _) => log::error!("Error getting workers: {e:?}"),
+            (Ok(new_workers), Some(old_workers)) if &new_workers == old_workers => {
+                log::debug!("Active worker set unchanged")
+            }
+            (Ok(new_workers), last_worker_set) => {
+                log::debug!("New set of active workers");
+                *last_worker_set = Some(new_workers.clone());
+                self.result_sender.send(new_workers).await?;
+            }
+        }
+        Ok(())
     }
 }

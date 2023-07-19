31334011d031c21f374a0c941f786e52c16178cd
ivan <ivanruch@gmail.com>
2023-07-19 16:05:59 +0200

    remove useless format

--- crates/persistent-storage/src/main.rs
+++ crates/persistent-storage/src/main.rs
@@ -4,7 +4,7 @@ use reqwest::Client;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
-    let hash = write_to_ipfs(client, format!("Foo")).await;
+    let hash = write_to_ipfs(client, "Foo".to_string()).await;
     println!("{:#?}", hash);
     Ok(())
 }

36992a91488fad07177ce7de9dc05f3cd23531c4
ivan <ivanruch@gmail.com>
2023-07-19 16:02:24 +0200

    fix error handling

--- crates/persistent-storage/src/lib.rs
+++ crates/persistent-storage/src/lib.rs
@@ -1,5 +1,6 @@
 use base64::{engine::general_purpose, Engine as _};
 use ethers::prelude::*;
+use ethers_signers::WalletError;
 use reqwest::{multipart, Client};
 use serde::Deserialize;
 
@@ -9,32 +10,32 @@ struct IpfsCreateResponse {
     Hash: String,
 }
 
-pub async fn write_to_ipfs(client: Client, file: String) -> Result<String, Err> {
-    let auth_key = get_auth_key().await;
+#[derive(Debug, thiserror::Error)]
+pub enum IPFSError {
+    #[error("Authentication message signature error: {0:?}")]
+    AuthSigning(#[from] WalletError),
+    #[error("Invalid Peer ID: {0:?}")]
+    Network(#[from] reqwest::Error),
+}
+
+pub async fn write_to_ipfs(client: Client, file: String) -> Result<String, IPFSError> {
+    let auth_key = get_auth_key().await?;
     let form = multipart::Form::new().text("file", file);
-    let res = match client
+    Ok(client
         .post("https://crustipfs.xyz/api/v0/add")
         .multipart(form)
         .bearer_auth(auth_key)
         .send()
-        .await
-    {
-        Ok(res) => res,
-        Err(e) => return Err(e),
-    };
-    match res.json::<IpfsCreateResponse>().await {
-        Ok(res) => res.Hash,
-        Err(e) => return Err(e),
-    }
+        .await?
+        .json::<IpfsCreateResponse>()
+        .await?
+        .Hash)
 }
 
-async fn get_auth_key() -> Result<String, Err> {
+async fn get_auth_key() -> Result<String, WalletError> {
     let wallet = Wallet::new(&mut rand::thread_rng());
     let address = format!("{:#?}", wallet.address());
-    let sig = match wallet.sign_message(&address).await {
-        Ok(sig) => sig,
-        Err(e) => return Err(e),
-    };
+    let sig = wallet.sign_message(&address).await?;
     let plain_auth_key = format!("eth-{address}:{sig}");
-    return general_purpose::STANDARD.encode(plain_auth_key.as_bytes());
+    Ok(general_purpose::STANDARD.encode(plain_auth_key.as_bytes()))
 }

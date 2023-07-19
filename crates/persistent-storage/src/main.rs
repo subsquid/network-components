03bd5705cb961a44e5ff48790da0f44c82cc2d90
ivan <ivanruch@gmail.com>
2023-07-19 14:58:11 +0200

    change content in ipfs example

--- crates/persistent-storage/src/main.rs
+++ crates/persistent-storage/src/main.rs
@@ -4,7 +4,7 @@ use reqwest::Client;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
-    let hash = write_to_ipfs(client, format!("Dupa")).await;
+    let hash = write_to_ipfs(client, format!("Foo")).await;
     println!("{:#?}", hash);
     Ok(())
 }

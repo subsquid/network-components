570de1e699925c9ee813ab4b82ae9b2d3216f980
ivan <ivanruch@gmail.com>
2023-07-20 14:54:21 +0200

    add url to struct

--- crates/persistent-storage/src/lib.rs
+++ crates/persistent-storage/src/lib.rs
@@ -4,7 +4,7 @@ use ethers_core::k256::ecdsa::{
     signature::hazmat::PrehashSigner, RecoveryId, Signature as RecoverableSignature,
 };
 use ethers_signers::WalletError;
-use reqwest::{multipart, Client};
+use reqwest::{multipart, Client, Url};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -14,7 +14,7 @@ struct IpfsCreateResponse {
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum IPFSError {
+pub enum CrustError {
     #[error("Authentication message signature error: {0:?}")]
     AuthSigning(#[from] WalletError),
     #[error("Invalid Peer ID: {0:?}")]
@@ -23,21 +23,37 @@ pub enum IPFSError {
 
 #[derive(Debug)]
 pub struct CrustClient<'a, D: Sync + Send + PrehashSigner<(RecoverableSignature, RecoveryId)>> {
-    client: &'a Client,
+    http_client: &'a Client,
     wallet: &'a Wallet<D>,
+    gateway_url: &'a Url,
 }
 
 impl<'a, D: Sync + Send + PrehashSigner<(RecoverableSignature, RecoveryId)>> CrustClient<'a, D> {
-    pub fn new(wallet: &'a Wallet<D>, client: &'a Client) -> CrustClient<'a, D> {
-        CrustClient { client, wallet }
+    pub fn new(
+        wallet: &'a Wallet<D>,
+        http_client: &'a Client,
+        gateway_url: &'a Url,
+    ) -> CrustClient<'a, D> {
+        CrustClient {
+            http_client,
+            wallet,
+            gateway_url,
+        }
     }
 
-    pub async fn write_to_ipfs(&self, file: &str) -> Result<String, IPFSError> {
+    // pub fn with_random_wallet() -> CrustClient<'a, SigningKey> {
+    //     let client = Client::new();
+    //     let wallet = Wallet::new(&mut rand::thread_rng());
+    //     let url = Url::parse("https://crustipfs.xyz").unwrap();
+    //     CrustClient::new(&wallet, &client, &url)
+    // }
+
+    pub async fn write_to_ipfs(&self, file: &str) -> Result<String, CrustError> {
         let auth_key = self.get_auth_key().await?;
         let form = multipart::Form::new().text("file", file.to_string());
         Ok(self
-            .client
-            .post("https://crustipfs.xyz/api/v0/add")
+            .http_client
+            .post(self.gateway_url.join("/api/v0/add").unwrap().as_str())
             .multipart(form)
             .bearer_auth(auth_key)
             .send()
--- crates/persistent-storage/src/main.rs
+++ crates/persistent-storage/src/main.rs
@@ -1,12 +1,13 @@
 use ethers::prelude::{rand, Wallet};
 use persistent_storage::CrustClient;
-use reqwest::Client;
+use reqwest::{Client, Url};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
     let wallet = Wallet::new(&mut rand::thread_rng());
-    let crust = CrustClient::new(&wallet, &client);
+    let gateway_url = Url::parse("https://crustipfs.xyz")?;
+    let crust = CrustClient::new(&wallet, &client, &gateway_url);
     let hash = crust.write_to_ipfs("Foo").await;
     println!("{:#?}", hash);
     Ok(())

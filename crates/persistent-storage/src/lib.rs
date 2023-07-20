2145faa33a316d0770470b52b98f490595d34490
ivan <ivanruch@gmail.com>
2023-07-20 16:08:07 +0200

    remove get_auth_key out of CrustClient struct

--- crates/persistent-storage/src/lib.rs
+++ crates/persistent-storage/src/lib.rs
@@ -1,5 +1,3 @@
-use crate::core::k256::ecdsa::SigningKey;
-
 use base64::{engine::general_purpose, Engine as _};
 use ethers::prelude::*;
 use ethers_core::k256::ecdsa::{
@@ -24,49 +22,52 @@ pub enum CrustError {
 }
 
 #[derive(Debug)]
-pub struct CrustClient<D: Sync + Send + PrehashSigner<(RecoverableSignature, RecoveryId)>> {
+pub struct CrustClient {
     http_client: Client,
-    wallet: Wallet<D>,
+    auth_key: String,
     gateway_url: Url,
 }
 
-impl<D: Sync + Send + PrehashSigner<(RecoverableSignature, RecoveryId)>> CrustClient<D> {
-    pub fn new(wallet: Wallet<D>, http_client: Client, gateway_url: Url) -> CrustClient<D> {
-        CrustClient {
+impl CrustClient {
+    pub async fn new<D: Sync + Send + PrehashSigner<(RecoverableSignature, RecoveryId)>>(
+        wallet: Wallet<D>,
+        http_client: Client,
+        gateway_url: Url,
+    ) -> Result<CrustClient, WalletError> {
+        Ok(CrustClient {
             http_client,
-            wallet,
+            auth_key: get_auth_key(wallet).await?,
             gateway_url,
-        }
+        })
+    }
+
+    pub async fn with_random_wallet() -> CrustClient {
+        let client = Client::new();
+        let wallet = Wallet::new(&mut rand::thread_rng());
+        let url = Url::parse("https://crustipfs.xyz").unwrap();
+        CrustClient::new(wallet, client, url).await.unwrap()
     }
 
     pub async fn write_to_ipfs(&self, file: &str) -> Result<String, CrustError> {
-        let auth_key = self.get_auth_key().await?;
         let form = multipart::Form::new().text("file", file.to_string());
         Ok(self
             .http_client
             .post(self.gateway_url.join("/api/v0/add").unwrap().as_str())
             .multipart(form)
-            .bearer_auth(auth_key)
+            .bearer_auth(&self.auth_key)
             .send()
             .await?
             .json::<IpfsCreateResponse>()
             .await?
             .hash)
     }
-
-    async fn get_auth_key(&self) -> Result<String, WalletError> {
-        let address = format!("{:#?}", self.wallet.address());
-        let sig = self.wallet.sign_message(&address).await?;
-        let plain_auth_key = format!("eth-{address}:{sig}");
-        Ok(general_purpose::STANDARD.encode(plain_auth_key.as_bytes()))
-    }
 }
 
-impl CrustClient<SigningKey> {
-    pub fn with_random_wallet() -> CrustClient<SigningKey> {
-        let client = Client::new();
-        let wallet = Wallet::new(&mut rand::thread_rng());
-        let url = Url::parse("https://crustipfs.xyz").unwrap();
-        CrustClient::new(wallet, client, url)
-    }
+async fn get_auth_key<D: Sync + Send + PrehashSigner<(RecoverableSignature, RecoveryId)>>(
+    wallet: Wallet<D>,
+) -> Result<String, WalletError> {
+    let address = format!("{:#?}", wallet.address());
+    let sig = wallet.sign_message(&address).await?;
+    let plain_auth_key = format!("eth-{address}:{sig}");
+    Ok(general_purpose::STANDARD.encode(plain_auth_key.as_bytes()))
 }

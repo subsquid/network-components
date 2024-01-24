a0c9b6a3e2866e7a2d41d303e2dfc77a3b5d4471
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2024-01-24 11:26:43 +0100

    Revert "New allocations contracts"
    This reverts commit 1a378c74857877840f1ee8d4fb58ba6ed4ee3ecd.

--- crates/contract-client/src/cli.rs
+++ crates/contract-client/src/cli.rs
@@ -1,4 +1,5 @@
 use clap::Args;
+use std::path::PathBuf;
 
 #[derive(Args, Default)]
 pub struct RpcArgs {
@@ -9,4 +10,26 @@ pub struct RpcArgs {
         default_value = "http://127.0.0.1:8545/"
     )]
     pub rpc_url: String,
+    #[arg(long, env, help = "Raw private key in hex format")]
+    pub wallet_key: Option<String>,
+    #[arg(
+        long,
+        env,
+        help = "Path to the keystore file (ignored if --wallet is set)"
+    )]
+    pub keystore_path: Option<PathBuf>,
+    #[arg(
+        long,
+        env,
+        help = "Password to decrypt the keystore",
+        default_value = ""
+    )]
+    pub keystore_password: String,
+    #[arg(
+        long,
+        env = "MAX_GET_LOG_BLOCKS",
+        help = "Maximum number of blocks to scan for events in a single RPC call",
+        default_value = "1000000000"
+    )]
+    pub max_log_blocks: u32,
 }

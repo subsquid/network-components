cda3caa5ab789a69bed555b63c980ad505c693cf
Dzmitry Kalabuk <d.kalabuk@subsquid.io>
2024-11-18 13:42:58 +0300

    Implement pull-based logs collector

--- crates/logs-collector/src/main.rs
+++ crates/logs-collector/src/main.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
-use std::time::Duration;
 
 use clap::Parser;
 use env_logger::Env;
+use sqd_network_transport::util::CancellationToken;
 use sqd_network_transport::{get_agent_info, AgentInfo, P2PTransportBuilder};
 
 use collector_utils::ClickhouseStorage;
@@ -22,6 +22,26 @@ use tikv_jemallocator::Jemalloc;
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
+fn create_cancellation_token() -> anyhow::Result<CancellationToken> {
+    use tokio::signal::unix::{signal, SignalKind};
+
+    let token = CancellationToken::new();
+    let copy = token.clone();
+    let mut sigint = signal(SignalKind::interrupt())?;
+    let mut sigterm = signal(SignalKind::terminate())?;
+    tokio::spawn(async move {
+        tokio::select!(
+            _ = sigint.recv() => {
+                copy.cancel();
+            },
+            _ = sigterm.recv() => {
+                copy.cancel();
+            },
+        );
+    });
+    Ok(token)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Init logger and parse arguments
@@ -34,20 +54,20 @@ async fn main() -> anyhow::Result<()> {
     // Build P2P transport
     let agent_info = get_agent_info!();
     let transport_builder = P2PTransportBuilder::from_cli(args.transport, agent_info).await?;
-    let contract_client: Arc<dyn sqd_contract_client::Client> =
-        transport_builder.contract_client().into();
-    let (incoming_messages, transport_handle) =
-        transport_builder.build_logs_collector(Default::default())?;
+    let contract_client: Arc<_> = transport_builder.contract_client().into();
+    let transport = transport_builder.build_logs_collector(Default::default())?;
 
     let storage = ClickhouseStorage::new(args.clickhouse).await?;
-    let logs_collector = LogsCollector::new(storage, contract_client.clone());
-    let storage_sync_interval = Duration::from_secs(args.storage_sync_interval_sec as u64);
-    let worker_update_interval = Duration::from_secs(args.worker_update_interval_sec as u64);
-    Server::new(incoming_messages, transport_handle, logs_collector)
+    let logs_collector = LogsCollector::new(storage);
+    let cancellation_token = create_cancellation_token()?;
+
+    Server::new(transport, logs_collector)
         .run(
             contract_client,
-            storage_sync_interval,
-            worker_update_interval,
+            args.collection_interval,
+            args.worker_update_interval,
+            args.concurrent_workers,
+            cancellation_token,
         )
         .await
 }

0c72184e4f305b8b299bd9fea07bcaeedce089bd
Denis Kanonik <denisbsu@gmail.com>
2024-12-10 13:44:39 +0000

    reverse assignments list before deleting

--- crates/network-scheduler/src/storage.rs
+++ crates/network-scheduler/src/storage.rs
@@ -379,6 +379,7 @@ impl S3Storage {
         for v in files
             .contents()
             .iter()
+            .rev()
             .skip(Config::get().assignment_history_len)
         {
             let Some(ref filepath) = v.key else {

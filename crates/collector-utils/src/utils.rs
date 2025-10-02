be7ae64a9053ae1ac7ffd7ea14646560f847f161
toschoosqd <t.schoofs@subsquid.io>
2025-10-02 12:57:39 +0100

    [i157-num-missing-chunks] parse assignment_id, store ones and assignment tp, rebased

--- crates/collector-utils/src/utils.rs
+++ crates/collector-utils/src/utils.rs
@@ -1,4 +1,5 @@
 use anyhow::anyhow;
+use chrono::DateTime;
 use std::time::UNIX_EPOCH;
 
 use base64::{prelude::BASE64_URL_SAFE_NO_PAD, Engine};
@@ -15,7 +16,7 @@ pub fn timestamp_now_ms() -> u64 {
 pub fn parse_assignment_id(aid: &str) -> anyhow::Result<u64> {
     let mut split = aid.split('_');
     if let Some(tp) = split.next() {
-        Ok(chrono::NaiveDateTime::parse_from_str(tp, "%Y%m%dT%H%M%S")?
+        Ok(chrono::NaiveDateTime::parse_from_str(tp, "%FT%T")?
             .and_utc()
             .timestamp_millis()
             .try_into()?)
@@ -35,18 +36,18 @@ mod test {
 
     #[test]
     fn test_parse_assignment_id() {
-        let sample = "20241008T141245_242da92f7d6c";
+        let hash = "C1A955A7E13FABEC64DCA7965104FA0CBF98C063A6FCB4473E243348CADFAFAE";
+        let sample = format!("2025-10-12T12:00:45_{hash}");
 
         let tp = parse_assignment_id(sample).expect("cannot parse sample");
 
-        let expected = Utc.with_ymd_and_hms(2024, 10, 8, 14, 12, 45).unwrap();
+        let expected = Utc.with_ymd_and_hms(2025, 10, 12, 12, 0, 45).unwrap();
         assert_eq!(expected.timestamp_millis() as u64, tp);
 
         let dt = DateTime::from_timestamp_millis(tp as i64).unwrap();
         assert_eq!(expected, dt);
 
-        let have = format!("{}_{}", dt.format("%Y%m%dT%H%M%S"), "242da92f7d6c");
+        let have = format!("{}_{}", dt.format("%FT%T"), hash);
         assert_eq!(sample, have);
-        
     }
 }
